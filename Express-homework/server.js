04b01b08194ff961521715e9b73b5ffd947c93c8
Wendeeznuts <dikwizera07@gmail.com>
2024-09-26 12:40:43 +0200

    Initial commit

---
+++ Express-homework/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('Hello, world! Welcome to my Express server.');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get('/about', (req, res) => {
+    res.send('This is the About page.');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('This is the Contact page.');
+});
+
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'This is the JSON data!', success: true });
+});
